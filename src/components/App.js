5eb43258673230ad015846327c93bb851973bde2
Olipolite <oliver.oquist3d@gmail.com>
2023-01-20 19:47:28 +0000

    added components and props for location details

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import '../styles/App.css';
 import React from 'react';
+import PropTypes from 'prop-types';
+import LocationDetails from './LocationDetails';
 
-function App() {
+function App({ location }) {
+  const { city, country } = location;
   return (
-    <div className="App">
-      <h1>Weather App!</h1>
-    </div>
+    <LocationDetails
+      city={city}
+      country={country}
+    />
   );
 }
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
