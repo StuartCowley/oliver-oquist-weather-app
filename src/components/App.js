8bcd8e3847be9852c0c6483ff1eb7274f49735f4
Olipolite <oliver.oquist3d@gmail.com>
2023-01-21 12:58:32 +0000

    added forecast component and tried using BEM

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,27 @@ import '../styles/App.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from './LocationDetails';
+import ForecastSummary from './ForecastSummary';
 
-function App({ location }) {
+function App({ location, forecasts }) {
   const { city, country } = location;
+  const {
+    date, temperature, description, icon,
+  } = forecasts;
+
   return (
-    <LocationDetails
-      city={city}
-      country={country}
-    />
+    <div>
+      <LocationDetails
+        city={city}
+        country={country}
+      />
+      <ForecastSummary
+        date={date}
+        temperature={temperature}
+        description={description}
+        icon={icon}
+      />
+    </div>
   );
 }
 
@@ -17,6 +30,12 @@ App.propTypes = {
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
+  }).isRequired,
+  forecasts: PropTypes.shape({
+    date: PropTypes.number,
+    temperature: PropTypes.number,
+    description: PropTypes.string,
+    icon: PropTypes.bool,
   }).isRequired,
 };
 
