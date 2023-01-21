49d337704ba182f0755ba6f9c09a69a195ac588d
Olipolite <oliver.oquist3d@gmail.com>
2023-01-21 11:25:07 +0000

    added test for city and location props, also updated app test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import React from "react";
 import App from '../components/App';
+import forecast from '../data/forecast.json';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Weather App/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("App", () => {
+  it('renders app component correctly', () => {
+    render(<App location={forecast.location} />);
+    const h1Element = screen.getByText(/Manchester, UK/i);
+    expect(h1Element).toBeInTheDocument();
+  });
 });
