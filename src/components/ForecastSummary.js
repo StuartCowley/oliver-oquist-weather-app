5a3699287f3a33deff66857c362fd80b331258ca
Olipolite <oliver.oquist3d@gmail.com>
2023-01-23 18:07:37 +0000

    finished the snapshot testing and values testing

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -3,21 +3,26 @@ import PropTypes from 'prop-types';
 
 function ForecastSummary(props) {
   const {
-    date, description, icon, temperature,
+    date,
+    temperature,
+    description,
+    icon,
   } = props;
+
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">
         {date}
       </div>
-      <div className="forecast-summary__description">
-        {description}
-      </div>
-      <div className="forecast-summary__icon">
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
         {icon}
       </div>
       <div className="forecast-summary__temperature">
-        {temperature}
+        {temperature.max}
+        &deg;C
+      </div>
+      <div className="forecast-summary__description">
+        {description}
       </div>
     </div>
   );
